b22434c2d21e5ba57890b3979c6cf305ee4f39ac
Dev Taylor <taylortech2024@gmail.com>
2025-08-28 20:17:36 -0300

    Criar Livro.java

---
+++ src/AtvdFacul/Livro.java
@@ -0,0 +1,33 @@
+package AtvdFacul
+
+class Livro {
+
+    String titulo;
+    String autor;
+    int anoPublicacao;
+
+    void exibirInfo() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Ano de Publicação: " + anoPublicacao);
+        System.out.println("------------------------------");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+   
+        Livro livro1 = new Livro();
+        livro1.titulo = "Dom Casmurro";
+        livro1.autor = "Machado de Assis";
+        livro1.anoPublicacao = 1899;
+
+        Livro livro2 = new Livro();
+        livro2.titulo = "O Hobbit";
+        livro2.autor = "J.R.R. Tolkien";
+        livro2.anoPublicacao = 1937;
+
+        livro1.exibirInfo();
+        livro2.exibirInfo();
+    }
+}
